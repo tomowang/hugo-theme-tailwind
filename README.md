bf54ab9b7a5f0524d563b19cf90bd19f07906ffc
Tomo Wang <tomo_wang@outlook.com>
2023-10-22 00:17:55 +0800

    feat: initial theme with basic layout and styles

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-# Theme Name
+# Hugo Theme Tailwind
 
 ## Features
 
 ## Installation
 
 ## Configuration
+
+## Development
+
+```bash
+pnpm install
+```
+
+* Development: `npm run dev`
+* Production: `npm run build`
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.html"],
+  theme: {
+    extend: {
+      typography: {
+        quoteless: {
+          css: {
+            'blockquote p:first-of-type::before': { content: 'none' },
+            'blockquote p:first-of-type::after': { content: 'none' },
+            'code::before': { content: 'none' },
+            'code::after': { content: 'none' },
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}

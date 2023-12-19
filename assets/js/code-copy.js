4d62bbaecc10084a6140a5de2193ad8f7c5ef9f8
Tomo Wang <tomo_wang@163.com>
2023-12-19 23:56:43 +0800

    feat: code copy to clipboard button

---
+++ assets/js/code-copy.js
@@ -0,0 +1,38 @@
+(function () {
+  const copyIcon = document.getElementById("code-copy").innerHTML;
+  const copyIconDone = document.getElementById("code-copy-done").innerHTML;
+
+  /**
+   * code inspired by https://digitaldrummerj.me/hugo-add-copy-code-snippet-button/
+   * and https://www.dannyguo.com/blog/how-to-add-copy-to-clipboard-buttons-to-code-blocks-in-hugo
+   */
+
+  function createCopyButton(highlightDiv) {
+    const button = document.createElement("button");
+    button.className = "right-1 top-1 absolute";
+    button.type = "button";
+    button.innerHTML = copyIcon;
+    button.addEventListener("click", () => copyCodeToClipboard(button, highlightDiv));
+    highlightDiv.insertBefore(button, highlightDiv.firstChild);
+  }
+
+  document.querySelectorAll(".highlight").forEach((highlightDiv) => createCopyButton(highlightDiv));
+
+  async function copyCodeToClipboard(button, highlightDiv) {
+    const codeToCopy = highlightDiv.querySelector("code").innerText;
+    try {
+      var result = await navigator.permissions.query({ name: "clipboard-write" });
+      if (result.state == "granted" || result.state == "prompt") {
+        await navigator.clipboard.writeText(codeToCopy);
+      }
+    } finally {
+      /* Chrome doesn't seem to blur automatically,
+         leaving the button in a focused state. */
+      button.blur();
+      button.innerHTML = copyIconDone;
+      setTimeout(function () {
+        button.innerHTML = copyIcon;
+      }, 2000);
+    }
+  }
+})();

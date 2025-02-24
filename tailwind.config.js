8e00710cbdc18bb502d1099cfdff0ff9cf34b4b6
Ben Reader <ben.reader@gmail.com>
2025-02-24 16:58:08 +0000

    fixing styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        fira: ["Fira Code", "monospace"],
+      },
       typography: {
         quoteless: {
           css: {

3cb995251ca6c45eab7fb03c3bee6af1051de47f
Ryan Peel <ryan.l.peel@gmail.com>
2025-02-26 23:20:29 -0600

    updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors 	updating colors

--- assets/js/search.js
+++ assets/js/search.js
@@ -41,18 +41,18 @@
   }
 
   const resultTemplate = `
-  <article class="flex flex-col gap-y-3 p-6 mt-6 mx-2 md:mx-0 rounded-lg shadow-md bg-white dark:bg-gray-700" id="result-{{= it.index }}">
-    <h2 class="text-4xl font-semibold text-slate-800 dark:text-slate-200">
+  <article class="flex flex-col gap-y-3 p-6 mt-6 mx-2 md:mx-0 rounded-lg shadow-md bg-white dark:bg-neutral-700" id="result-{{= it.index }}">
+    <h2 class="text-4xl font-semibold text-neutral-800 dark:text-neutral-200">
       <a href="{{= it.permalink }}">{{! it.title }}</a>
     </h2>
-    <h3 class="my-4 text-large text-slate-600 dark:text-slate-300">
+    <h3 class="my-4 text-large text-neutral-600 dark:text-neutral-300">
       {{! it.snippet }}
     </h3>
-    <ul class="flex flex-row flex-wrap text-slate-500 dark:text-slate-300">
+    <ul class="flex flex-row flex-wrap text-neutral-500 dark:text-neutral-300">
       {{~ it.categories :v }}
       <li>
         <span
-          class="text-sm mr-2 px-2 py-1 rounded border border-emerald-800 bg-emerald-800 text-slate-50">
+          class="text-sm mr-2 px-2 py-1 rounded border border-emerald-800 bg-emerald-800 text-neutral-50">
           {{!v}}
         </span>
       </li>
